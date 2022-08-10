65d8efa153fac7c165a7daabfd68c0b2f7d0f202
Rasheed <shaikrasheed3014@gmail.com>
2022-08-10 10:59:32 +0530

    Add. LinkedList example of List interface

---
+++ src/list/LinkedListExample.java
@@ -0,0 +1,32 @@
+package list;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+
+        System.out.println("Add 30, 20, 10, 40 elements to the list");
+        list.add(30);
+        list.add(20);
+        list.add(10);
+        list.add(40);
+
+        System.out.println("Read all the elements of list");
+        System.out.println(list);
+
+        System.out.println("Update 20 to 15 in list");
+        list.set(1, 15);
+        System.out.println(list);
+
+        System.out.println("Delete 10 element from list");
+        list.remove(Integer.valueOf(10));
+        System.out.println(list);
+
+        System.out.println("Sort the list");
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
